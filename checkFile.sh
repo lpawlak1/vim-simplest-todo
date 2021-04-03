fce8acf0a2dd060311fe76421736cbf54dc2eb10
lpawlak1 <lukaszpawlak162@gmail.com>
2021-04-03 12:38:40 +0200

    Deleted unnecesary files

--- checkFile.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-
-touch tod.md
-chmod +x bin/checkFile
-./bin/checkFile
-cat tod.md > todo.md
-rm tod.md
-exit 0
--- deleteDone.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-
-touch tod.md
-chmod +x bin/deleteDone
-./bin/deleteDone
-cat tod.md > todo.md
-rm tod.md
-exit 0
