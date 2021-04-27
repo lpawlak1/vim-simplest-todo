192ddebefd053cacac44c02c245b0b03b0a7ffd3
Steven <44085060+aconitumnapellus@users.noreply.github.com>
2021-04-27 11:53:45 +0000

    Rework marks list
    Remove strip approach and get marks with | as seperator from a list

--- save.py
+++ save.py
@@ -1,8 +1,9 @@
 marks = open("marks.md", "r")
 out = open("todo.md", "w+")
 in_ = open("tod.md", "r")
-done_mark = marks.readline().strip()
-undone_mark = marks.readline().strip()
+marks_list= marks.readline().split("|")
+done_mark = marks_list[0].strip()
+undone_mark = marks_list[1].strip()
 marks.close()
 for line in in_.readlines():
     if undone_mark in line:
